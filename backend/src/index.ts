6a56851defc3af8ade478efcd21345d6784d6a79
swami-hai-ham <swamiatharva15104@gmail.com>
2025-06-13 20:11:10 +0530

    uptimerobot health

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
-import userRouter from './routes/user'
-import workerRouter from './routes/worker'
-import cors from 'cors';
+import userRouter from "./routes/user";
+import workerRouter from "./routes/worker";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/v1/user', userRouter)
-app.use('/v1/worker', workerRouter)
+app.use("/v1/user", userRouter);
+app.use("/v1/worker", workerRouter);
 
-app.listen(3003)
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(3003);
